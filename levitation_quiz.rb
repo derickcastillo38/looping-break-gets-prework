6c869ae567d6097f75c8331678dc56fad301aff4
derickcastillo38 <9derick@gmail.com>
2018-06-21 22:17:23 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,7 @@
 def levitation_quiz
 	#your code here
   puts "What is the spell that enacts levitation?"
-  response = gets.chomp
+  response = gets
   unless response == "Wingardium Leviosa"
     levitation_quiz
   else
